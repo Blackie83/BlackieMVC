bba21b6ee155481833d9bc83c4d39cb7e927d0fd
jgswart <jgswart@gmail.com>
2016-07-26 06:08:18 +0200

    testing

--- BlackieMVC/Models/Administration_Unit.cs
+++ BlackieMVC/Models/Administration_Unit.cs
@@ -4,6 +4,7 @@ namespace BlackieMVC.Models
 {
   public class Administration_Unit
   {
+    string test;
     public int Id { get; set; }
     public string Name { get; set; }
     public DateTime CreatedDate { get; set; }
