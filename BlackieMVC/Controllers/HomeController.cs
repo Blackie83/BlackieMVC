caec3fd6a7123affa660c7ed098e0e6e04b4f25c
jgswart <jgswart@gmail.com>
2016-07-26 13:59:53 +0200

    20160726 1359

--- BlackieMVC/Controllers/HomeController.cs
+++ BlackieMVC/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace BlackieMVC.Controllers
 
     public ActionResult About()
     {
-      ViewBag.Message = "Your application description page.";
+      ViewBag.Message = "Your application description page. testing";
 
       return View();
     }
